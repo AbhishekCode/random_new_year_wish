286cb775b15d46725c5da4e8305e4cab92c59717
abhishek singh <abhishekcode1@gmail.com>
2016-01-01 14:49:43 +0530

    Merge remote-tracking branch 'origin/master'

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var msgs = [
     "Many years have passed with big resolutions & regrets. Let this be the year you make a difference to your life & lifes around you #Happy2016",
     "Happy New year, have a blast",
+    "Ten, nine, eight, seven, six, five, four, three, two, one, yippee! It’s a New Year! Happy New Year, my beloved friend.",
+    "May your hair, your teeth, your face-lift, your abs and your stocks not fall, and may your blood pressure, your triglycerides, your cholesterol, your white blood count and your mortgage interest never rise.",
+    "Many people await New Year’s Day to make a new start to their old habits. I wish you otherwise. Happy New Year.",
+    "HCounting my blessings and wishing you more. Hope you enjoy the New Year in store. Have a joyous New Year, my dear friend!",
+    "Out with the old, in with the new: may you be happy the whole year through. Happy New Year!",
     "Happy new year to all you lovely people.",
     "Today, is the first blank page of a 365-page book. Write a good one.",
     "HAPPY NEW YEAR! MAKE SURE TO SPREAD POSITIVITY AND MAKE THIS YEAR A GOOD ONE! #HappyNewYear",
@@ -15,10 +20,25 @@ var msgs = [
 
 function getMessage () {
     var index = getRandomInt(0 , this.msgs.length-1);
-   document.getElementById("demo").innerHTML = msgs[3]; 
-    
+   document.getElementById("demo").innerHTML = msgs[index]; 
+     stWidget.addEntry({
+                 "service":"sharethis",
+                 "element":document.getElementById('button_1'),
+                 "url":"http://sharethis.com",
+                 "title":"sharethis",
+                 "type":"large",
+                 "text":"ShareThis" ,
+                 "image":"http://www.softicons.com/download/internet-icons/social-superheros-icons-by-iconshock/png/256/sharethis_hulk.png",
+                 "summary":"this is description1"
+         });
 }
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function copyText () {
+      var holdtext =  document.getElementById("demo").innerText;
+      Copied = holdtext.createTextRange();
+      Copied.execCommand("Copy");
 }
